f637f11927b81a2a4228a72c8971089e70ea6279
manyamdeepika <dr6358212@gmail.com>
2026-01-29 22:52:00 +0530

    trial

--- practiceproject.py
+++ practiceproject.py
@@ -6,17 +6,5 @@ ilist=0
 qlist=0
 plist=0
 finalprice=0
-for i in range(items(len)):
-    inpt=input("if you want to buy press 2 or 3 for quit")
-    if(inpt==2):
-        item=input("enter an item")
-        quantity=int(input("enter quantity"))
-        if item in items.keys():
-        price=quantity*items[item]
-        pricelist.append((item,items,quantity,price))
-        totalprice+=price
-        ilist.append(item)
-        qlist.append(quantity)
-        plist.append(price) 
-        finalprice+=price
+
     
